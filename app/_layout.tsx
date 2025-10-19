f0bbb759cc2c7f1442e63c397e3cc0a8c47f3eaa
tct123 <42028373+tct123@users.noreply.github.com>
2025-10-19 12:22:26 +0200

    .

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,12 @@
-import isDarkMode from "@/components/CheckDarkMode";
 import { i18n } from "@/utils/mylocalisation";
-import { Ionicons } from "@expo/vector-icons";
-import { Tabs } from "expo-router";
+import { NativeTabs } from "expo-router/unstable-native-tabs";
 
 export default function RootLayout() {
   let title = i18n.t("title")
   let about = i18n.t("about")
-  return (<Tabs screenOptions={{ sceneStyle: { backgroundColor: isDarkMode() ? "black" : "white" }, headerStyle: { backgroundColor: isDarkMode() ? "black" : "white" }, tabBarStyle: { backgroundColor: isDarkMode() ? "black" : "white" }, headerTitleStyle: { color: isDarkMode() ? "white" : "black" } }}>
-    <Tabs.Screen name="index" options={{ title: title, tabBarIcon: ({ color, size }) => (<Ionicons name="home" color={color} size={size} />) }} />
-    <Tabs.Screen name="about" options={{ title: about, tabBarIcon: ({ color, size }) => (<Ionicons name="information" color={color} size={size} />) }} />
-  </Tabs>
+  return (<NativeTabs>
+    <NativeTabs.Trigger name="index" options={{ title: title }} />
+    <NativeTabs.Trigger name="about" options={{ title: about }} />
+  </NativeTabs>
   );
 }
