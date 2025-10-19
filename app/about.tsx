d5506449d883505bac4a566c7ed0b59137ca1624
tct123 <42028373+tct123@users.noreply.github.com>
2025-10-19 12:07:45 +0200

    Update about.tsx

--- app/about.tsx
+++ app/about.tsx
@@ -1,10 +1,8 @@
 import isDarkMode from "@/components/CheckDarkMode";
 import { Link } from "expo-router";
 import { StyleSheet, Text, View } from "react-native";
-import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 export default function Index() {
-    const insets = useSafeAreaInsets()
     const styles = StyleSheet.create({
         safeareaview: { flex: 1 },
         view: {
