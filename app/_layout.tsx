d8982d52e246041e6c6fdd6fd20256a8513f7d0f
tct123 <42028373+tct123@users.noreply.github.com>
2025-10-19 12:31:46 +0200

    integrated native tabs

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,16 @@
 import { i18n } from "@/utils/mylocalisation";
-import { NativeTabs } from "expo-router/unstable-native-tabs";
+import { Icon, NativeTabs } from "expo-router/unstable-native-tabs";
 
 export default function RootLayout() {
   let title = i18n.t("title")
   let about = i18n.t("about")
   return (<NativeTabs>
-    <NativeTabs.Trigger name="index" options={{ title: title }} />
-    <NativeTabs.Trigger name="about" options={{ title: about }} />
+    <NativeTabs.Trigger name="index" options={{ title: title }}>
+      <Icon sf={"house.fill"} drawable="ic_menu_home" />
+    </NativeTabs.Trigger>
+    <NativeTabs.Trigger name="about" options={{ title: about }}>
+      <Icon sf={"house.fill"} drawable="ic_menu_home" />
+    </NativeTabs.Trigger>
   </NativeTabs>
   );
 }
