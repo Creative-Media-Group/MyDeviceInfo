18612f49bebbe516f355801479fa43f1ed3bcd70
tct123 <42028373+tct123@users.noreply.github.com>
2026-07-14 10:28:58 +0200

    Update index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -1,47 +1,41 @@
 import * as Device from 'expo-device';
 import useStyles from '../styles/mystyles';
-import { useRouter } from 'expo-router';
+import { useRouter, useTheme } from 'expo-router';
 import { Button, Host, List, ListItem, Text } from "@expo/ui"
+import { useState } from 'react';
 
+const DATA = [
+  { name: "Manufacturer", value: Device.manufacturer },
+  { name: "Modelname", value: Device.modelName },
+  { name: "Brand", value: Device.brand },
+  { name: "OS Name", value: Device.osName },
+  { name: "OS Version", value: Device.osVersion },
+  { name: "Platform API level", value: Device.platformApiLevel },
+  { name: "Productname", value: Device.productName },
+  { name: "Devicename", value: Device.deviceType },
+  { name: "Supported CPU architectures", value: Device.supportedCpuArchitectures },
+  { name: "Total memory", value: Device.totalMemory }
+]
 export default function Index() {
-  const styles = useStyles();
   const router = useRouter();
   return (
     <Host
-      //style={styles.homeview}
-      matchContents={true}
+      style={{ flex: 1 }}
+    //style={styles.homeview}
+    //matchContents={true}
     >
       <List>
-        <ListItem>
-          <Text >Manufacturer: {Device.manufacturer}</Text>
-        </ListItem>
-        <ListItem>
-          <Text >Modelname: {Device.modelName}</Text>
-        </ListItem>
-        <ListItem>
-          <Text >Brand: {Device.brand}</Text>
-        </ListItem>
-        <ListItem>
-          <Text >OS Name: {Device.osName}</Text>
-        </ListItem>
-        <ListItem>
-          <Text >OS Version: {Device.osVersion}</Text>
-        </ListItem>
-        <ListItem>
-          <Text >Platform API level: {Device.platformApiLevel}</Text>
-        </ListItem>
-        <ListItem>
-          <Text >Productname: {Device.productName}</Text>
-        </ListItem>
-        <ListItem>
-          <Text >Devicename: {Device.deviceType}</Text>
-        </ListItem>
-        <ListItem>
-          <Text >Supported CPU architectures: {Device.supportedCpuArchitectures}</Text>
-        </ListItem>
-        <ListItem>
-          <Text >Total memory: {Device.totalMemory}</Text>
-        </ListItem>
+        {DATA.map(item => (
+          <ListItem
+            supportingText={<Text>{item.value?.toString()}</Text>}
+            key={item.name}
+            onPress={
+              () => { }
+            }
+          >
+            {item.name}
+          </ListItem>
+        ))}
         <ListItem>
           <Button label='Details' onPress={() => { router.push('/details') }} />
         </ListItem>
